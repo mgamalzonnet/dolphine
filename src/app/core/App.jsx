194b03d4a53349893877a849ebad5700e4054bde
M gamal home <mgamal@z0n.net>
2025-09-05 13:42:39 +0300

    ss

--- src/app/core/App.jsx
+++ src/app/core/App.jsx
@@ -2,11 +2,11 @@ import { useLanguageDirection } from "@/hooks/useLanguageDirection";
 
 // App Components
 import AppProviders from "./AppProviders";
-// import AppRoutes from "./AppRoutes";
+import AppRoutes from "./AppRoutes";
 
-// // Global Components
-// import GlobalLoader from "@/components/feedback/GlobalLoader";
-// import ModalManager from "@/components/feedback/modal/ModalManager";
+// Global Components
+import GlobalLoader from "@/components/feedback/GlobalLoader";
+import ModalManager from "@/components/feedback/modal/ModalManager";
 // import { useModal } from "@/components/feedback/modal/useModal";
 
 const App = () => {
@@ -24,9 +24,15 @@ const App = () => {
   // } = useModal();
 
   return (
-    <AppProviders/>
-    //   <AppRoutes />
-    // </AppProviders>
+    <AppProviders>
+      <div className="app-container">
+        {/* Global Components */}
+        <GlobalLoader />
+        <ModalManager />
+
+        <AppRoutes />
+      </div>
+    </AppProviders>
   );
 };
 
--- src/app/core/AppProviders.jsx
+++ src/app/core/AppProviders.jsx
@@ -1,18 +1,16 @@
 import { Provider as ReduxProvider } from "react-redux";
 import { I18nextProvider } from "react-i18next";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 // Local imports
 import store from "@/store";
 import i18n from "@/i18n";
 
-const AppProviders = () => (
+const AppProviders = ({ children }) => (
   <BrowserRouter>
     {/* <ReduxProvider store={store}>
       <I18nextProvider i18n={i18n}> */}
-        <Routes>
-          <Route path="/" element={<div>Home Page</div>} />
-        </Routes>
+        {children}
       {/* </I18nextProvider>
     </ReduxProvider> */}
   </BrowserRouter>
