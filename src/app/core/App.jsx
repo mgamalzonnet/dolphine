998dbe7b5609b51259693703bc006146043559ca
M gamal home <mgamal@z0n.net>
2025-09-05 12:51:18 +0300

    err

--- src/app/core/App.jsx
+++ src/app/core/App.jsx
@@ -1,17 +1,17 @@
-import { useLanguageDirection } from "@/hooks/useLanguageDirection";
+// import { useLanguageDirection } from "@/hooks/useLanguageDirection";
 
 // App Components
 import AppProviders from "./AppProviders";
 // import AppRoutes from "./AppRoutes";
 
 // Global Components
-import GlobalLoader from "@/components/feedback/GlobalLoader";
-import ModalManager from "@/components/feedback/modal/ModalManager";
+// import GlobalLoader from "@/components/feedback/GlobalLoader";
+// import ModalManager from "@/components/feedback/modal/ModalManager";
 // import { useModal } from "@/components/feedback/modal/useModal";
 
 const App = () => {
   // Initialize language direction
-  useLanguageDirection();
+  // useLanguageDirection();
 
   return (
     <AppProviders>
