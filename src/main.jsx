0130cfdeea2e0323a42b6c87968a88e48521b55f
M gamal home <mgamal@z0n.net>
2025-09-05 11:48:17 +0300

    fix strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./i18n"; // Initialize i18n
 import App from "./app/core/App";
+import React from "react";
 
 createRoot(document.getElementById("root")).render(
-    <App /> 
+   <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
